bd2774faf7adc442f2fa2f1fc8071e9c232efee2
any-One <oussamakouraich@gmail.com>
2023-07-28 20:34:11 +0100

    FIX compilation errors

--- test/main.c
+++ test/main.c
@@ -11,6 +11,8 @@ int main(void)
 {
     int len;
     int len2;
+    unsigned int ui;
+    ui = (unsigned int)INT_MAX + 1024;
 
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
@@ -18,6 +20,8 @@ int main(void)
     printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
     printf("Negative:[%d]\n", -762534);
+    _printf("Unsigned:[%u]\n", ui);
+    printf("Unsigned:[%u]\n", ui);
     _printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
